2c85770b9f6b6d433a523d16afc0bb2ab4eac948
Grahambone37 <electricgrunt37@gmail.com>
2023-01-11 17:00:36 -0800

    made other routes

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.get('/', (req, res) => {
     res.send('Bread Home Page')
 })
 
+const breadsController = require('./controllors/breads_controller.js')
+app.use('/breads', breadsController)
+
+app.get('*', (req, res) => {
+    res.status(404).send('Error 404, no page available.')
+})
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 })
